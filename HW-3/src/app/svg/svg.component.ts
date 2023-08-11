5aa1970eaa2150740e030373f6bb536447dcecf0
Alex43961 <semen43961@gmail.com>
2023-08-11 20:22:33 +0300

    added s-th HW-3

--- HW-3/src/app/svg/svg.component.ts
+++ HW-3/src/app/svg/svg.component.ts
@@ -6,14 +6,33 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./svg.component.css']
 })
 export class SVGComponent {
-height = "600";
-width = "600";
-fill = "orange";
-fontSize = "30";
-stroke = "green";
-circleRadius = 40;
-onMouseWheel(event: WheelEvent) {
-  this.circleRadius += event.deltaY > 0 ? -2 : 2;
-  if (this.circleRadius < 5) this.circleRadius = 5;
-}
+  height = "500";
+  width = "500";
+  fill = "orange";
+  fontSize = "30";
+  stroke = "green";
+  circleRadius = 100;
+  isTransparent: boolean = false;
+  rectangleFill = "aliceblue"
+
+
+  onMouseWheel(event: WheelEvent) {
+    this.circleRadius += event.deltaY > 0 ? -10 : 10;
+    if (this.circleRadius < 5) {
+      this.circleRadius = 5;
+    } else if (this.circleRadius > 250) {
+      this.circleRadius = 250;
+    }
+  }
+  toggleOpacity() {
+    this.isTransparent = !this.isTransparent;
+  }
+
+  randomBackground() {
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+
+    this.rectangleFill = randomColor;
+  }
 }
